b719708712cad1743a6c03afccd6d0883d490b20
Ivan Tomilov <tiabc.work@gmail.com>
2017-02-05 21:27:51 +0300

    Added DeliveryStatus tests

--- sms_test.go
+++ sms_test.go
@@ -1,5 +1,47 @@
 package sms
 
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeliveryStatus_IsDelivered(t *testing.T) {
+	s := DeliveryStatus(StatusDelivered)
+	require.True(t, s.IsDelivered())
+	require.False(t, s.IsInProgress())
+	require.False(t, s.IsUndelivered())
+}
+
+func TestDeliveryStatus_IsInProgress(t *testing.T) {
+	statuses := []DeliveryStatus{
+		StatusQueued,
+		StatusSent,
+	}
+	for _, s := range statuses {
+		t.Run("check status " + string(s), func(t *testing.T) {
+			require.True(t, s.IsInProgress())
+			require.False(t, s.IsDelivered())
+			require.False(t, s.IsUndelivered())
+		})
+	}
+}
+
+func TestDeliveryStatus_IsUndelivered(t *testing.T) {
+	statuses := []DeliveryStatus{
+		StatusUndeliveredUnavailable,
+		StatusUndeliveredSpam,
+		StatusUndeliveredInvPhone,
+		"AbsolutelyUnknownStatus",
+	}
+	for _, s := range statuses {
+		t.Run("check status " + string(s), func(t *testing.T) {
+			require.True(t, s.IsUndelivered())
+			require.False(t, s.IsInProgress())
+			require.False(t, s.IsDelivered())
+		})
+	}
+}
+
 /*
 TODO: Write tests.
 
