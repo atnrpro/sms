d8073bb3c49f7fc26093f58cd4280ea5514f780a
Ivan Tomilov <tiabc.work@gmail.com>
2017-02-09 00:06:24 +0300

    Added comments for a test and performed little refactoring

--- sms_test.go
+++ sms_test.go
@@ -8,16 +8,6 @@ import (
 	"io/ioutil"
 )
 
-func TestNewSender(t *testing.T) {
-	s := NewSender("login", "passwordMD5")
-	require.Equal(t, Sender{
-		Login:       "login",
-		PasswordMD5: "passwordMD5",
-		SandboxMode: false,
-		Client:      http.DefaultClient,
-	}, s)
-}
-
 func TestDeliveryStatus_IsDelivered(t *testing.T) {
 	s := DeliveryStatus(StatusDelivered)
 	require.True(t, s.IsDelivered())
@@ -55,22 +45,46 @@ func TestDeliveryStatus_IsUndelivered(t *testing.T) {
 	}
 }
 
+func TestNewSender(t *testing.T) {
+	// Act.
+	s := NewSender("login", "passwordMD5")
+
+	// Assert.
+	require.Equal(t, Sender{
+		Login:       "login",
+		PasswordMD5: "passwordMD5",
+		SandboxMode: false,
+		Client:      http.DefaultClient,
+	}, s)
+}
+
 func TestParseStatusResponse_Success(t *testing.T) {
+	// Arrange.
 	req := bytes.NewBufferString("1\n3")
 	s := Sender{}
+
+	// Act.
 	ds, err := s.parseStatusResponse(req)
+
+	// Assert.
 	require.Nil(t, err)
 	require.Equal(t, DeliveryStatus(StatusDelivered), ds)
 }
 
 func TestParseStatusResponse_BadCode(t *testing.T) {
+	// Arrange.
 	req := bytes.NewBufferString("Unexpected response")
 	s := Sender{}
+
+	// Act.
 	_, err := s.parseStatusResponse(req)
+
+	// Assert.
 	require.NotNil(t, err)
 }
 
 func TestParseSendSMSResponse_Success(t *testing.T) {
+	// Arrange.
 	req := bytes.NewBufferString(`1
 123
 1
@@ -83,7 +97,11 @@ Array
  [sendTime] => 2016-10-16 15:00:00
 )`)
 	s := Sender{}
+
+	// Act.
 	r, err := s.parseSendSMSResponse(req)
+
+	// Assert.
 	require.Nil(t, err)
 	require.Equal(t, "123", r.SMSID)
 	require.Equal(t, 1, r.SMSCnt)
@@ -93,20 +111,30 @@ Array
 }
 
 func TestParseSendSMSResponse_Error(t *testing.T) {
+	// Arrange.
 	req := bytes.NewBufferString(`-1
 Invalid login/password`)
 	s := Sender{}
+
+	// Act.
 	_, err := s.parseSendSMSResponse(req)
+
+	// Assert.
 	require.NotNil(t, err)
 }
 
 func TestParseSendSMSResponse_MalformedRequest(t *testing.T) {
+	// Arrange.
 	req := bytes.NewBufferString(`1
 123
 1
 `)
 	s := Sender{}
+
+	// Act.
 	_, err := s.parseSendSMSResponse(req)
+
+	// Assert.
 	require.NotNil(t, err)
 }
 
@@ -122,6 +150,7 @@ func (c *fakeClient) Do(req *http.Request) (*http.Response, error) {
 }
 
 func TestRequest(t *testing.T) {
+	// Arrange.
 	c := &fakeClient{}
 	s := Sender{
 		Login:       "+79998887766",
@@ -130,8 +159,10 @@ func TestRequest(t *testing.T) {
 		Client:      c,
 	}
 
+	// Act.
 	_, _ = s.request("/send", map[string]string{"user-arg": "user-val"})
 
+	// Assert.
 	require.Equal(t, "https", c.req.URL.Scheme)
 	require.Equal(t, "sms-rassilka.com", c.req.URL.Host)
 	require.Equal(t, "/api/simple/send", c.req.URL.Path)
